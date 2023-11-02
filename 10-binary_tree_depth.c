7fe7604a2f4700df966a1b47a6967c35937bce9b
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-02 15:55:46 +0100

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,18 +8,8 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
-	}
 
-	size_t depth = 0;
-
-	while (tree != NULL)
-	{
-		depth++;
-		tree = tree->parent;
-	}
-
-	return (depth);
+	return (1 + binary_tree_depth(tree->parent));
 }
